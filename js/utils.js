3e4e204a25cc61bc03fb73466884087a521ee142
onaisahmed <99874813+onaisahmed@users.noreply.github.com>
2022-05-24 01:28:00 +0500

    Add files via upload

---
+++ js/utils.js
@@ -0,0 +1,36 @@
+function rectangularCollision({rectangle1, rectangle2}) {
+    return(rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
+        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
+        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
+        && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+        && rectangle1.isAttacking)
+}
+
+function determineWinner({ player, enemy, timerId}) {
+    clearTimeout(timerId)
+    document.querySelector('.result').style.display = 'flex'
+    if(player.health > enemy.health){
+        document.querySelector('.result').innerHTML = 'Musashi Wins'
+        enemy.switchSprites('death')
+    } else if(enemy.health > player.health){
+        document.querySelector('.result').innerHTML = 'Kojiro Wins'
+        player.switchSprites('death')
+    } else if(player.health === enemy.health) {
+        document.querySelector('.result').innerHTML = 'Tie'
+        player.switchSprites('death')
+        enemy.switchSprites('death')
+    }
+}
+let timer = 60
+let timerId
+function decreaseTimer() {
+    if(timer > 0) {
+    timerId = setTimeout(decreaseTimer, 1000)
+    timer--
+    document.querySelector('.timer').innerHTML = timer
+    }
+
+    if(timer === 0) {
+        determineWinner({ player, enemy, timerId })
+    }
+}
